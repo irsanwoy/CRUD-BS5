f0e39e497b8808b1963a0f09180a99a5dc9bd021
IrsanWoy! <105915685+irsanwoy@users.noreply.github.com>
2023-05-07 19:03:22 +0700

    Update and rename kelola.html to kelola.php

--- kelola.php
+++ kelola.php
@@ -17,7 +17,8 @@
       </div>
     </nav>
     <div class="container">
-      <div class="mb-3 row">
+     <form method="POST" action="proses.php">
+     <div class="mb-3 row">
         <label for="nisn" class="col-sm-2 col-form-label"
           >NISN</label
         >
@@ -32,37 +33,42 @@
         <div class="col-sm-10">
           <input type="text" class="form-control" id="nama" placeholder="Ex : Alexander" />
         </div>
-        <div class="mb-3 row mt-3">
-            <label for="jkel" class="col-sm-2 col-form-label">
-              Jenis Kelamin
-            </label>
-            <div class="col-sm-10">
-                <select id="jkel" class="form-select">
-                    <option selected>Jenis Kelamin</option>
-                    <option value="1">Laki-laki</option>
-                    <option value="2">Perempuan</option>
-                  </select>
-            </div>
-            <div class="mb-3">
-                <label for="foto" class="form-label">Foto Siswa</label>
-                <input class="form-control" type="file" id="foto">
-              </div>
-          </div>
+      </div>
+      <div class="mb-3 row mt-3">
+        <label for="jkel" class="col-sm-2 col-form-label">
+          Jenis Kelamin
+        </label>
+        <div class="col-sm-10">
+          <select id="jkel" class="form-select">
+            <option selected>Jenis Kelamin</option>
+            <option value="1">Laki-laki</option>
+            <option value="2">Perempuan</option>
+          </select>
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="foto" class="form-label">Foto Siswa</label>
+        <input class="form-control" type="file" id="foto">
       </div>
       <div class="mb-3">
         <label for="alamat" class="form-label">Alamat lengkap</label>
         <textarea class="form-control" id="alamat" rows="3"></textarea>
       </div>
-    </div>
     <div class="mb-3 row mt-4">
-        <div class="col">
-            <button href="index.html" class="btn btn-primary">
-                Tambahkan
-            </button>
-            <a href="index.html" class="btn btn-danger">
-                Batal
-            </a>
-        </div>
+      <?php if(isset($_GET['ubah'])){ ?>
+      <div class="col">
+        <button type="submit" name="aksi" value="edit" class="btn btn-primary">Simpan Perubahan</button>
+      </div>
+      <?php } else { ?>
+      <div class="col-3">
+        <button type="submit" name="aksi" value="add" class="btn btn-primary">Tambahkan</button>
+      </div>
+      <?php } ?>
+      <div class="col3 mt-3">
+        <a href="index.php" class="btn btn-danger">Batal</a>
+      </div>
+    </div>
+     </form>
     </div>
   </body>
 </html>
